95f7eb5df14135b95099fdd0f79c41a6f98bae4e
Adrien Siebert <adrien.siebert@gmail.com>
2012-07-03 16:55:03 +0200

    First version of the linput program, refactored from bench-put

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-lindory-put
-===========
+# Lindory put
+
+Linput is a small benchmark/test utility for the Lindory project.
+
+It can create a fake RDF/skos dataset or load a custom one, and load it into Lindory. It also reports
+the insertion progress according to the server response, and measures the time for each operation. 
+
+## Installation
+Use [npm](http://npmjs.org) to install:
+    $ npm install -g lindory-put
+
+## Usage
+
+    $ linput [OPTIONS]
+
+
+
+* `-p | --port <port_number>`: port of lindory/tapis server (default valut is set to apache port).
+Port number can be replaced by 'apache' or 'restify', in order to use the pkgi.env values.
+* `-s | --size <box_number>`: size of generated data (number of boxes, default value is set to 50).
+Strings 'small', 'medium' and 'big' can also be used.
+* `-f | --force`: force generating data (generate a file "/dataset/bench-put.xml").
+* `-c | --clean`: clean lindory after bench, by removing the container(s).
+* `-C | --copy`: requests a copy of the bench container once uploaded.
+* `-l | --load <FILE>`: loads a custom XML file to send to the server. This disables any "-f | --force"
+option, preventing Linput from overwriting your custom file.
+
+
+## Output 
+
+The command will display the progress of the operation, indicating the number of
+characters sent by the server, which should be equal to the number of boxes processed.
+If any, an error counter is displayed, showing the number of "x" received.
+
+Upon completion of the test, the following information will be written for each request
+(PUT and DELETE):
+
+* Status Code of the response
+* Start and end date
+* Duration of the request
+
+
+# License
+
+[MIT/X11](./LICENSE)
---
+++ bin/linput.js
@@ -0,0 +1,438 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+var http = require('http');
+var path = require('path');
+var url = require('url');
+var util = require('util');
+var url = require('url');
+var zlib = require('zlib');
+
+var async = require('async');
+var colors = require('colors');
+var faker = require('Faker');
+var optimist = require('optimist').demand(1);
+var prompt = require('prompt');
+var __ = require('underscore');
+
+var tl = new (require('../lib/timelogger.js'))();
+var env = {}; //FIXME toremove
+
+
+var optiargs = {
+  'u': {
+    alias: 'user',
+    string: true,
+    description: 'User name and password to use for authentication, as: "user:password"' },
+  's': {
+    alias: 'size',
+    default: '100',
+    description: 'Size of generated data (number of boxes)' },
+  'f': {
+    alias: 'force',
+    boolean: true,
+    description: 'Force generating data' },
+  'l': {
+    alias: 'load',
+    description: 'Load and send the given file (disables -f parameter)' },
+  'c': {
+    alias: 'clean',
+    boolean: true,
+    description: 'Clean lindory after bench, by removing container(s)' },
+  'C': {
+    alias: 'copy',
+    description: 'Perform a copy test on the uploaded container' },
+  'h': {
+    alias: 'help',
+    boolean: true }
+}
+
+optimist.options(optiargs);
+optimist.usage('Usage: $0 [-u user:password] TARGET_URL [OPTIONS]')
+var argv = optimist.argv;
+
+/*******************************
+ * Process parameters
+ */
+if (argv.h) {
+  console.log(optimist.help());
+  return;
+}
+
+// Host and port
+var targetUrl = url.parse(argv._.pop());
+
+// User authentication
+var userName = "";
+var userPass = "";
+
+if (typeof(argv.user) === "string") {
+  var userSplit = argv.user.split(':');
+  userName = userSplit.shift();
+  userPass = userSplit.shift();
+}
+else {
+  userName = "test";
+}
+
+
+// Dataset size
+if (argv.s === 'small')   { argv.s = 50; }
+if (argv.s === 'medium')  { argv.s = 500; }
+if (argv.s === 'big')     { argv.s = 5000; }
+argv.s = Number(argv.s);
+
+// Paths
+var filePath = "/tmp/linput-data.xml";
+if (argv.l) { filePath = argv.l; }
+
+var createString = 'Data generation';
+var putString = 'Put request';
+var copyString = 'Put copy request';
+
+
+
+
+/*******************************
+ * Password prompt
+ */
+var promptPassword = function (callback) {
+  prompt.start();
+  var promptOptions = [{
+    name: 'password',
+    hidden: true,
+    description: 'Password for user "' + userName + '"'
+  }];
+      
+  prompt.get(promptOptions, function (err, result) {
+    if (err) {
+      callback('Error');
+      return;
+    }
+    userPass = result.password;
+    callback();
+  });
+}
+
+
+/**
+ * DATA GENERATION
+ */
+var createFakeData = function (callback) {
+  tl.start(createString);
+  console.log('# Creating fake data');
+  
+  var file = fs.openSync(filePath, 'w');
+  var buffer = "";
+ 
+  fs.writeSync(file, '<?xml version="1.0" encoding="UTF-8"?>' + "\n"
+    + '<rdf:RDF xmlns:skos="http://www.w3.org/2004/02/skos/core#"'
+    + 'xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">' + "\n");
+  
+  for (var x = 0; x < argv.s; x++) {
+    buffer += "\t" + '<skos:Concept rdf:about="' + x + '">' + "\n";
+    buffer += "\t\t" + '<skos:prefLabel xml:lang="fr">' +
+        faker.Name.findName() + '</skos:prefLabel>' + "\n";
+    buffer += "\t\t" + '<skos:altLabel xml:lang="fr">' +
+        faker.Lorem.sentence() + '</skos:altLabel>' + "\n";
+    if (x % 5 === 1) {
+      buffer += "\t\t" + '<skos:definition xml:lang="fr">' +
+        faker.Lorem.paragraphs() + '</skos:definition>' + "\n";
+    }
+    buffer += "\t\t" + '<skos:prefLabel xml:lang="en">' +
+        faker.Name.findName() + '</skos:prefLabel>' + "\n";
+    buffer += "\t\t" + '<skos:altLabel xml:lang="en">' +
+        faker.Lorem.sentence() + '</skos:altLabel>' + "\n";
+    if (x % 10 === 1) {
+      buffer += "\t\t" + '<skos:definition xml:lang="en">' +
+        faker.Lorem.paragraphs() + '</skos:definition>' + "\n";
+    }
+    if (x % 10 === 0) {
+      buffer += "\t\t" + '<skos:broader rdf:resource="./' + (x + 10) + '"/>' + "\n";
+    }
+    if (x % 15 === 0) {
+      buffer += "\t\t" +
+        '<skos:narrower rdf:resource="http://' + faker.Internet.domainName() +
+        "/" + faker.Internet.domainWord() + '"/>' + "\n";
+    }
+    buffer += "\t</skos:Concept>\n";
+    
+    
+    fs.writeSync(file, buffer);
+    buffer = "";
+    
+    process.stdout.write('\r' + (1 + x) + ' / ' + argv.s + ' boxes');
+  }
+  
+  fs.writeSync(file, "</rdf:RDF>\n");
+  fs.fsyncSync(file);
+
+  tl.stop(createString);
+  console.log();
+  callback();
+}
+
+
+/**
+ * REQUESTS
+ */
+
+//
+// PUT request function
+//
+var putRequest = function (callback) {
+  tl.start(putString);
+  var fileSize = fs.statSync(filePath).size;
+  
+  console.log("# Sending bench file of size " + fileSize + " to "
+      + targetUrl.hostname.bold + " (port " + targetUrl.port + ")");
+  var firstDataReceived = false;
+  
+  //Options for the PUT request
+  var putOptions = {
+    host           : targetUrl.hostname,
+    port           : targetUrl.port,
+    path           : targetUrl.path,
+    method         : 'PUT',
+    auth           :  userName + ':' + userPass,
+    headers        : {
+      'Connection' : 'keep-alive',
+      'Content-type': 'text/xml'
+    }
+  };
+  
+  // Request declaration and events
+  var request = http.request(putOptions, function (response) {
+    response.on('end', function () {
+      tl.stop(putString);
+      console.log('\nPut response: ' + response.statusCode);
+      
+      callback();
+    });
+  });
+  
+  request.on('response', function (response) {
+    if (!firstDataReceived) {
+      console.log("Time to receive a response: ".grey + tl.getTimeString(putString).grey);
+      firstDataReceived = true;
+    }
+    
+    var count = 0;
+    var errCount = 0;
+    response.on('data', function (chunk) {
+      if (chunk == 'x') {
+        errCount++;
+      }
+      count++;
+      var errorMessage = (errCount > 0 ? " (" + errCount + " errors)" : "");
+      process.stdout.write('\r\t' + count.toString().cyan + errorMessage.yellow
+          + ' → ' + chunk + "       | " + tl.getTimeString(putString).grey + "      ");
+    });
+  });
+  
+  request.on('error', function (e) {
+    callback('Problem with PUT request: ' + e.message);
+  });
+  
+  request.setSocketKeepAlive(true);
+  request.setNoDelay();
+  
+  
+  // File stream declaration and reading events
+  var inputStream = fs.createReadStream(filePath); //zlib.createGunzip()???
+  inputStream.on('data', function (data) {
+    request.write(data);
+  });
+  //No event on close
+  inputStream.on('error', function (err) {
+    request.abort();
+    callback("PUT error when reading file: " + err);
+  });
+  
+};
+
+// PUT copy request
+var copyRequest = function (callback) {
+  console.log('# Copy container: ');
+  tl.start(copyString);
+  var firstDataReceived = false;
+  
+  var copyOptions = {
+    host           : targetUrl.hostname,
+    port           : targetUrl.port,
+    path           : '/lincopy.xml',
+    method         : 'PUT',
+    auth           : userName + ':' + userPass,
+    headers        : {
+      'Connection' : 'keep-alive',
+      'content-type': 'text/x-url'
+    }
+  };
+  
+  var request = http.request(copyOptions, function (response) {
+    response.on('end', function () {
+      tl.stop(copyString);
+      console.log('\nCopy response: ' + response.statusCode);
+      
+      callback();
+    });
+  });
+  request.write('/bench-put.xml'); //URL à copier
+  
+  request.on('response', function (response) {
+    if (!firstDataReceived) {
+      console.log("Time to receive a response: ".grey + tl.getTimeString(copyString).grey);
+      firstDataReceived = true;
+    }
+    
+    var count = 0;
+    var errCount = 0;
+    response.on('data', function (chunk) {
+      if (chunk == 'x') {
+        errCount++;
+      }
+      count++;
+      var errorMessage = (errCount > 0 ? " (" + errCount + " errors)" : "");
+      process.stdout.write('\r\t' + count.toString().cyan + errorMessage.yellow
+          + ' → ' + chunk + "       | " + tl.getTimeString(copyString).grey + "      ");
+    });
+  });
+  
+  request.on('error', function (e) {
+    callback('Problem with PUT copy request: ' + e.message);
+  });
+  
+  request.setSocketKeepAlive(true);
+  request.setNoDelay();
+  request.end();
+};
+
+
+
+
+//
+// DELETE request function
+//
+var delRequest = function (callback) {
+  process.stdout.write('# Delete test container: ');
+  var delOptions = {
+    host           : targetUrl.hostname,
+    port           : targetUrl.port,
+    path           : targetUrl.path,
+    method         : 'DELETE',
+    auth           : userName + ':' + userPass,
+    headers        : { 'Connection' : 'keep-alive' }
+  };
+  
+  var request = http.request(delOptions, function (response) {
+    response.on('end', function () {
+      console.log(response.statusCode);
+      
+      callback();
+    });
+  });
+  request.on('error', function (e) {
+    callback('Problem with DELETE request: ' + e.message);
+  });
+  
+  request.setSocketKeepAlive(true);
+  request.setNoDelay();
+  request.end();
+};
+
+// DELETE copy request
+var delCopyRequest = function (callback) {
+  process.stdout.write('# Delete copy container: ');
+  
+  var delCopyOptions = {
+    host           : targetUrl.hostname,
+    port           : targetUrl.port,
+    path           : '/lincopy.xml',
+    method         : 'DELETE',
+    auth           : userName + ':' + userPass,
+    headers        : { 'Connection' : 'keep-alive' }
+  };
+  
+  var request = http.request(delCopyOptions, function (response) {
+    response.on('end', function () {
+      console.log(response.statusCode);
+      
+      callback();
+    });
+  });
+  request.on('error', function (e) {
+    callback('Problem with DELETE copy request: ' + e.message);
+  });
+  
+  request.setSocketKeepAlive(true);
+  request.setNoDelay();
+  request.end();
+};
+
+
+//
+// Final callback, displays results
+//
+var endCallback = function (err, results) {
+  
+  if (err) {
+    console.log("\n# ".red + err.red.bold);
+  }
+  else {
+    __.each(tl.listAction, function (values, key) {
+      console.log("\t" + key.cyan + ": "
+          + values.duration.bold
+          + " \t (" + values.start.toLocaleTimeString() + " - "
+          + values.end.toLocaleTimeString() + ")");
+    });
+  }
+  
+  console.log("# End of bench\n");
+}
+
+
+
+
+/**
+ * PROCESSING
+ */
+
+var waterfallFunctions = [];
+
+
+if (userPass === undefined || userPass === "") {
+  waterfallFunctions.push(promptPassword);
+}
+
+//Check if the test data file exists
+var fileExist = true;
+try {
+  fileExist = fs.statSync(filePath).isFile();
+} catch (e) {
+  fileExist = false;
+  if (argv.l) {
+    console.log('# Error: the given file doesn\'t exist.'.red);
+    return;
+  }
+}
+
+if ((argv.f || !fileExist) && argv.l === undefined) {
+  waterfallFunctions.push(createFakeData);        //Create data
+}
+
+waterfallFunctions.push(putRequest);              //Send PUT
+
+if (argv.copy) {
+  waterfallFunctions.push(copyRequest);           //Send PUT copy
+  
+  if (argv.clean) {
+    waterfallFunctions.push(delCopyRequest);      // Send DELETE copy
+  }
+}
+
+if (argv.clean) {
+  waterfallFunctions.push(delRequest);            //Send DELETE
+}
+
+async.waterfall(waterfallFunctions, endCallback);
+
